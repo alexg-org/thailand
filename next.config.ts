4513816c463f28cfc9e6cd4b3e588f4705815935
Alex <alex@Alexs-MBP.lan>
2025-06-08 19:50:36 +0300

    tets

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    PORT: '3000'
+  },
+  poweredByHeader: false,
+  reactStrictMode: true,
 };
 
 export default nextConfig;
